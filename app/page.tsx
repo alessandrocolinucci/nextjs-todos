0a8f2c4ae80a6c4564bbf5c239e699ad0a380222
Alessandro Colinucci <info@acolinucci.com>
2023-05-24 16:37:13 +0200

    home: page updated to query todos

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
-const Home = () => {
+import Link from "next/link";
+import { prisma } from "./database/db";
+
+const Home = async () => {
+
+  const todos = await prisma.todo.findMany();
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <header className="flex items-center justify-between w-full py-2">
+        <h1 className="underline">Home - Todos</h1>
+        <Link href={'/new'} className="flex items-center gap-2 py-2 px-4 rounded-md transition hover:bg-slate-700">Add new</Link>
+      </header>
+
+      <main className="py-4">
+        <h3 className="mb-2">Here your todo list</h3>
+        <ul className="pl-4">
+          {todos.map(todo => (
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ul>
+      </main>
+    </>
   )
 }
 
