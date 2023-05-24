937cd7b3a52b507f6e08e3997b708b31c6d26942
Alessandro Colinucci <info@acolinucci.com>
2023-05-24 17:32:42 +0200

    fix: minor fixes in TodoItem component

--- app/components/TodoItem.tsx
+++ app/components/TodoItem.tsx
@@ -8,7 +8,7 @@ const TodoItem = ({ id, title, completed }: TodoItemProps) => {
   return (
     <li className="flex gap-1 items-center">
       <input id={id} className="cursor-pointer peer h-4 w-4" type="checkbox" checked={completed} />
-      <label htmlFor={id} className="peer-checked:line-through">{title}</label>
+      <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">{title}</label>
     </li>
   )
 }
